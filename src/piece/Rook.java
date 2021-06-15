f527e5e3f3d7049eaeb53fd0b503cd7ee03533ef
jevtovicc <jevtovic99@yahoo.com>
2021-06-15 19:07:00 +0200

    implement rook movement logic

--- src/piece/Rook.java
+++ src/piece/Rook.java
@@ -1,8 +1,10 @@
 package piece;
 
 import color.PieceColor;
+import gui.GameWindow;
 import gui.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -12,6 +14,71 @@ public class Rook extends Piece {
 
     @Override
     public List<Position> getAvailablePositions() {
-        return null;
+
+        List<Position> positions = new ArrayList<>();
+
+        Position currPosition = getPosition();
+
+        int rowIncrement = 1;
+
+        while (currPosition.getRow() + rowIncrement <= 8) {
+            Position temp = GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() + rowIncrement);
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+            rowIncrement++;
+        }
+
+        rowIncrement = 1;
+
+        while (currPosition.getRow() - rowIncrement >= 1) {
+            Position temp = GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() - rowIncrement);
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+            rowIncrement++;
+        }
+
+        int colIncrement = 1;
+
+        while (currPosition.getColumn() + colIncrement <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + colIncrement), currPosition.getRow());
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+            colIncrement++;
+        }
+
+        colIncrement = 1;
+
+        while (currPosition.getColumn() - colIncrement >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - colIncrement), currPosition.getRow());
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+            colIncrement++;
+        }
+
+        return positions;
     }
 }
