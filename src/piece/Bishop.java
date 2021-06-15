9048de0d00eab6af848754dea22076b5f7fa099c
jevtovicc <jevtovic99@yahoo.com>
2021-06-15 18:24:28 +0200

    implement bishop movement logic

--- src/piece/Bishop.java
+++ src/piece/Bishop.java
@@ -1,8 +1,10 @@
 package piece;
 
 import color.PieceColor;
+import gui.GameWindow;
 import gui.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece {
@@ -13,6 +15,90 @@ public class Bishop extends Piece {
 
     @Override
     public List<Position> getAvailablePositions() {
-        return null;
+
+        List<Position> positions = new ArrayList<>();
+
+        Position currPosition = getPosition();
+
+        int rowIncrement = 1, colIncrement = 1;
+
+        while (currPosition.getRow() + rowIncrement <= 8 && currPosition.getColumn() + colIncrement <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + colIncrement), currPosition.getRow() + rowIncrement);
+
+            // break if obstacle piece is found
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+
+            rowIncrement++;
+            colIncrement++;
+        }
+
+        rowIncrement = 1;
+        colIncrement = 1;
+
+        while (currPosition.getRow() + rowIncrement <= 8 && currPosition.getColumn() - colIncrement >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - colIncrement), currPosition.getRow() + rowIncrement);
+
+            // break if obstacle piece is found
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+
+            rowIncrement++;
+            colIncrement++;
+        }
+
+        rowIncrement = 1;
+        colIncrement = 1;
+
+        while (currPosition.getRow() - rowIncrement >= 1 && currPosition.getColumn() + colIncrement <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + colIncrement), currPosition.getRow() - rowIncrement);
+
+            // break if obstacle piece is found
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+
+            rowIncrement++;
+            colIncrement++;
+        }
+
+        rowIncrement = 1;
+        colIncrement = 1;
+
+        while (currPosition.getRow() - rowIncrement >= 1 && currPosition.getColumn() - colIncrement >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - colIncrement), currPosition.getRow() - rowIncrement);
+
+            // break if obstacle piece is found
+            if (temp.getPiece().isPresent()) {
+                if (temp.getPiece().get().getColor() != getColor()) {
+                    positions.add(temp);
+                }
+                break;
+            } else {
+                positions.add(temp);
+            }
+
+            rowIncrement++;
+            colIncrement++;
+        }
+
+        return positions;
     }
 }
