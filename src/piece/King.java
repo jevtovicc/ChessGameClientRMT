567718b9ff7ded5a7a4b2f2f58ac9fa059a87c85
jevtovicc <jevtovic99@yahoo.com>
2021-06-15 18:37:14 +0200

    implement king movement logic

--- src/piece/King.java
+++ src/piece/King.java
@@ -1,8 +1,10 @@
 package piece;
 
 import color.PieceColor;
+import gui.GameWindow;
 import gui.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece {
@@ -12,6 +14,90 @@ public class King extends Piece {
 
     @Override
     public List<Position> getAvailablePositions() {
-        return null;
+        List<Position> positions = new ArrayList<>();
+
+        Position currPosition = getPosition();
+
+        if (currPosition.getRow() + 1 <= 8) {
+            Position temp = GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() + 1);
+            temp.getPiece()
+                .ifPresentOrElse(piece -> {
+                    if (piece.getColor() != getColor()) {
+                        positions.add(temp);
+                    }
+                }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getRow() - 1 >= 1) {
+            Position temp = GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() - 1);
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getColumn() + 1 <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + 1), currPosition.getRow());
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getColumn() - 1 >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - 1), currPosition.getRow());
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getRow() + 1 <= 8 && currPosition.getColumn() + 1 <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + 1), currPosition.getRow() + 1);
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getRow() + 1 <= 8 && currPosition.getColumn() - 1 >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - 1), currPosition.getRow() + 1);
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getRow() - 1 >= 1 && currPosition.getColumn() + 1 <= 'h') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() + 1), currPosition.getRow() - 1);
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        if (currPosition.getRow() - 1 >= 1 && currPosition.getColumn() - 1 >= 'a') {
+            Position temp = GameWindow.getPositionAt((char)(currPosition.getColumn() - 1), currPosition.getRow() - 1);
+            temp.getPiece()
+                    .ifPresentOrElse(piece -> {
+                        if (piece.getColor() != getColor()) {
+                            positions.add(temp);
+                        }
+                    }, () -> positions.add(temp));
+        }
+
+        return positions;
     }
 }
