54bf9eb84804210e7007d5d966cf684d3b518497
jevtovicc <jevtovic99@yahoo.com>
2021-06-15 11:40:43 +0200

    add missing files to previous commit

---
+++ src/piece/BlackPawn.java
@@ -0,0 +1,64 @@
+package piece;
+
+import color.PieceColor;
+import gui.GameWindow;
+import gui.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlackPawn extends Pawn {
+
+    public BlackPawn(PieceColor c, String pngFilePath) {
+        super(c, pngFilePath);
+    }
+
+    @Override
+    public void move(Position destination) {
+        super.move(destination);
+        setMoved(true);
+    }
+
+    @Override
+    public List<Position> getAvailablePositions() {
+
+        List<Position> positions = new ArrayList<>();
+
+        Position currPosition = getPosition();
+
+        if (!hasMoved()) {
+            positions.add(GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() + 2));
+        }
+
+        // straight by one position
+        Position destination = GameWindow.getPositionAt(currPosition.getColumn(), currPosition.getRow() + 1);
+
+        if (destination.getPiece().isEmpty()) {
+            positions.add(destination);
+        }
+
+        // diagonally to eat opposite piece
+        if (currPosition.getRow() + 1 <= 8) {
+            if (currPosition.getColumn() - 1 >= 'a') {
+                Position pos = GameWindow.getPositionAt((char)(currPosition.getColumn() - 1), currPosition.getRow() + 1);
+                pos.getPiece()
+                        .ifPresent(currPiece -> {
+                            if (currPiece.getColor() != getColor()) {
+                                positions.add(pos);
+                            }
+                        });
+            }
+            if (currPosition.getColumn() + 1 <= 'h') {
+                Position pos = GameWindow.getPositionAt((char)(currPosition.getColumn() + 1), currPosition.getRow() + 1);
+                pos.getPiece()
+                        .ifPresent(currPiece -> {
+                            if (currPiece.getColor() != getColor()) {
+                                positions.add(pos);
+                            }
+                        });
+            }
+        }
+
+        return positions;
+    }
+}
